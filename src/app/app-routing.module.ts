24dceab7e94afaea75e47435ff3eb3b7908f1937
IntegrityTek123 <76465468+IntegrityTek123@users.noreply.github.com>
2020-12-23 18:20:02 +0530

    events and aboutus

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { EventsComponent } from './events/events.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+// const routes: Routes = [{path:'',component:MenuComponent, children:[{path:'',component:AboutUsComponent},{path:"event",component:EventsComponent}]}];
+const routes: Routes = [
+  {path:'',component:AboutUsComponent},
+  {path:"event",component:EventsComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatExpansionModule, MatInputModule} from '@angular/material'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MenuComponent } from './menu/menu.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { EventsComponent } from './events/events.component';
+import { RegistrationComponent } from './registration/registration.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AboutUsComponent,
+    EventsComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+      MatExpansionModule,
+       MatInputModule,
+      FormsModule,
+      ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
